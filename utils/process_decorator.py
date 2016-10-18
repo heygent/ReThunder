0daf0fb596fb26483f565f4ebbb11c276e98e8f6
Emanuele Gentiletti <emanuele.gentiletti@gmail.com>
2016-10-18 13:11:34 +0200

    Adds attribute-unbound run_process_with_env decorator factory

--- utils/process_decorator.py
+++ utils/process_decorator.py
@@ -1,7 +1,15 @@
+from functools import wraps
 
-def process(process_fn):
 
-    def wrapper(self, *args, **kwargs):
-        return self.env.process(process_fn(self, *args, **kwargs))
+def run_process(process_fn, env_attr='env'):
+
+    @wraps(process_fn)
+    def wrapper(self, *args):
+        return getattr(self, env_attr).process(process_fn(self, *args))
 
     return wrapper
+
+
+def run_process_with_env(env_attr: str):
+    return lambda fn: run_process(fn, env_attr)
+
