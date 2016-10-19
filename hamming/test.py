f3db8ae60db5cce676564f3f4c980338e998f6db
Emanuele Gentiletti <emanuele.gentiletti@gmail.com>
2016-10-19 17:54:09 +0200

    Refactors test parts and uses relative imports

--- hamming/test.py
+++ hamming/test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import unittest
-import wrappers as hamming
+from . import encode, decode
 from itertools import product
 
 
@@ -10,8 +10,8 @@ class HammingTest(unittest.TestCase):
     def test_no_errors(self):
 
         for i in range(1 << 11):
-            encoded = hamming.encode(i)
-            errors, decoded = hamming.decode(encoded)
+            encoded = encode(i)
+            errors, decoded = decode(encoded)
             self.assertEqual((0, i), (errors.value, decoded))
 
     def test_one_error(self):
@@ -20,27 +20,27 @@ class HammingTest(unittest.TestCase):
 
             for i in range(1 << 11):
 
-                encoded = hamming.encode(i)
+                encoded = encode(i)
 
                 encoded ^= 1 << error_position
 
-                errors, decoded = hamming.decode(encoded)
+                errors, decoded = decode(encoded)
                 self.assertEqual((1, i), (errors.value, decoded))
 
     def test_two_errors(self):
 
-        for error_position_1, error_position_2 in product(range(11), range(11)):
+        for error_pos_1, error_pos_2 in product(range(11), range(11)):
 
-            if error_position_1 == error_position_2:
+            if error_pos_1 == error_pos_2:
                 continue
 
             for i in range(1 << 11):
 
-                encoded = hamming.encode(i)
+                encoded = encode(i)
 
-                encoded ^= (1 << error_position_1) | (1 << error_position_2)
+                encoded ^= (1 << error_pos_1) | (1 << error_pos_2)
 
-                errors, decoded = hamming.decode(encoded)
+                errors, decoded = decode(encoded)
                 self.assertEqual(2, errors.value)
 
 
