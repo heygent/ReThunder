066001db5ceeb412220403d8b313557d12c8441b
Emanuele Gentiletti <emanuele.gentiletti@gmail.com>
2016-11-15 11:56:34 +0100

    Adds slave_node.py

---
+++ protocol/slave_node.py
@@ -0,0 +1,56 @@
+from collections import defaultdict
+
+import simpy
+
+from protocol.packet import Packet, PacketCodes, HelloRequestPacket
+from protocol.rethunder_node import ReThunderNode
+from utils.run_process_decorator import run_process
+
+HELLO_TIMEOUT = 500
+
+
+class SlaveNode(ReThunderNode):
+
+    def __init__(self, env: simpy.Environment, transmission_speed,
+                 physical_address):
+
+        super().__init__(env, transmission_speed)
+
+        self.static_address = None
+        self.dynamic_address = None
+        self.physical_address = physical_address
+        self.noise_table = defaultdict(lambda: 0)
+        self.last_sent_noise_table = None
+
+        self.hello_timeout = HELLO_TIMEOUT
+        self.run_until = lambda: False
+
+    @run_process
+    def hello_proc(self):
+
+        hello_packet = HelloRequestPacket()
+        hello_packet.physical_address = self.physical_address
+
+        while not self.run_until():
+
+            # todo usa unita' di misura coerente per la lunghezza dei messaggi
+            yield self._send_to_network_proc(
+                hello_packet, hello_packet.number_of_frames()
+            )
+
+            received = yield self._receive_packet_proc(self.hello_timeout)
+
+            if received.code == PacketCodes.hello_response:
+                raise NotImplemented
+
+    @run_process
+    def run_proc(self):
+
+        yield self.hello_proc()
+
+        while not self.run_until():
+
+            received = yield self._receive_packet_proc()
+
+
+
