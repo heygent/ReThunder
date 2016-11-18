b7879e874b3de962a66632de0fba70dd53ec2f4e
Emanuele Gentiletti <emanuele.gentiletti@gmail.com>
2016-11-18 23:34:10 +0100

    Adds tracer.py

--- protocol/tracer.py
+++ protocol/tracer.py
@@ -2,9 +2,15 @@ import enum
 
 
 class TracerCodes(enum.Enum):
-    new_address = 01
-    
+    new_address = 0b01
+    static_addressing = 0b10
+
 
 class Tracer:
-    def __init__(self):
-        self.code
+
+    def __init__(self, code, offset):
+        self.code = code
+        self.offset = offset
+
+    def number_of_frames(self):
+        return 2 if self.offset.bit_length() > 9 else 1
