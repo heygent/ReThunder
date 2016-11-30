eda49dd77dcf42a91165fa9b848be0152823e661
Emanuele Gentiletti <emanuele.gentiletti@gmail.com>
2016-11-30 10:52:15 +0100

    Trims last lint

--- utils/iterblocks.py
+++ utils/iterblocks.py
@@ -5,4 +5,3 @@ def iterblocks(iterable, block_size, intersection_size=0):
 
     iters = [islice(iterable, i, None) for i in range(block_size)]
     return islice(zip(*iters), 0, None, block_size - intersection_size)
-
