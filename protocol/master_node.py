8d6dc1eb451f2667163ec738196137ebf544fb23
Emanuele Gentiletti <emanuele.gentiletti@gmail.com>
2016-12-30 17:38:15 +0100

    Changes MasterNode to use acks, estimate return time and changes send function to be a process

--- protocol/master_node.py
+++ protocol/master_node.py
@@ -1,5 +1,6 @@
 import logging
 import itertools
+from collections import namedtuple
 from copy import deepcopy
 from typing import List, Dict
 
@@ -7,17 +8,24 @@ import networkx as nx
 from networkx.algorithms import bipartite
 import simpy
 
+from infrastructure.message import make_transmission_delay
 from protocol.packet import Packet, RequestPacket, ResponsePacket
-from protocol.rethunder_node import ReThunderNode
+from protocol.rethunder_node import ReThunderNode, ACK_TIMEOUT, RETRANSMISSIONS
 from protocol.node_data_manager import NodeDataManager, NodeDataT
 from protocol.tracer import Tracer
 from utils.condition_var import BroadcastConditionVar
 from utils.func import singledispatchmethod
 from utils.iterblocks import iterblocks
+from utils.preemption_first_resource import PreemptionFirstResource
 from utils.run_process_decorator import run_process
 from utils.graph import shortest_paths_tree, preorder_tree_dfs
 
 logger = logging.getLogger(__name__)
+
+
+AnswerPendingRecord = namedtuple(
+    'AnswerPendingRecord', 'token, path, expiring_time'
+)
 
 
 class BusyError(Exception):
@@ -34,16 +42,17 @@ class MasterNode(ReThunderNode):
         self.on_message_received = on_message_received
         self._sptree = None             # type: nx.DiGraph
         self._shortest_paths = None     # type: Dict[NodeDataT, List[NodeDataT]]
+        self._free_network_res = PreemptionFirstResource(self.env)
         self._send_cond = BroadcastConditionVar(self.env)
-        self._current_message = None
-        self._current_message_path = None
+        self._answer_pending = None
         self._node_manager = NodeDataManager()
+        self._token_it = itertools.cycle(range(1 << Packet.token.max_bits))
 
     def __repr__(self):
         return '<MasterNode>'
 
     def init_from_netgraph(self, netgraph: nx.Graph, initial_noise_value=0.5,
-                           assign_logic_addr=True):
+                           **kwargs):
 
         addr_graph = bipartite.projected_graph(
             netgraph, (node for node in netgraph.nodes_iter()
@@ -54,11 +63,11 @@ class MasterNode(ReThunderNode):
         nx.relabel_nodes(addr_graph, lambda x: x.static_address, False)
 
         return self.init_from_static_addr_graph(
-            addr_graph, initial_noise_value, assign_logic_addr
+            addr_graph, initial_noise_value, **kwargs
         )
 
     def init_from_static_addr_graph(self, addr_graph, initial_noise_value=0.5,
-                                    assign_logic_addr=True):
+                                    **kwargs):
 
         if not 0 <= initial_noise_value <= 2:
             raise ValueError('initial_noise_value must be between 0 and 2')
@@ -80,6 +89,8 @@ class MasterNode(ReThunderNode):
 
         self.node_graph = node_graph
         self._update_sptree()
+
+        assign_logic_addr = kwargs.get('assign_logic_addr', True)
 
         if not assign_logic_addr:
             return
@@ -155,15 +166,23 @@ class MasterNode(ReThunderNode):
                     ancestor_of_previous
                 )
 
-    def send_message(self, message, message_length,
-                     destination_static_addr: int):
+    @run_process
+    def send_message_proc(self, message, message_length,
+                          destination_static_addr: int, preempt=False):
 
-        if self._current_message is not None:
-            raise BusyError("{} is waiting for another message "
-                            "response.".format(self))
+        env = self.env
 
-        self._send_cond.broadcast((message, message_length,
-                                   destination_static_addr))
+        with self._free_network_res.request(preempt) as req:
+
+            yield req
+            self._send_cond.broadcast((message, message_length,
+                                       destination_static_addr))
+            try:
+                yield env.timeout(self._answer_pending.expiring_time - env.now)
+                self._answer_pending = None
+            except simpy.Interrupt:
+                logger.warning("{} request message was preempted before "
+                               "expiry.")
 
     @run_process
     def run_proc(self):
@@ -190,16 +209,30 @@ class MasterNode(ReThunderNode):
 
             if send_ev.processed:
 
-                assert self._current_message is None, (
-                    "{} has not been prevented to send a message while "
-                    "waiting for a response, so it is in an invalid state."
-                    .format(self))
+                msg, msg_len, dest = send_ev.value
+                path_to_dest = self._shortest_paths[dest]
 
-                packet = self._make_request_packet(*send_ev.value)
-                self._send_to_network_proc(packet, packet.number_of_frames())
+                packet = self._make_request_packet(msg, msg_len, path_to_dest)
+                acknowledged = (yield from self._send_and_acknowledge(packet))
+
+                if acknowledged:
+
+                    estimated_rtt = (
+                        len(path_to_dest) *
+                        ACK_TIMEOUT *
+                        RETRANSMISSIONS *
+                        make_transmission_delay(self._transmission_speed,
+                                                packet.number_of_frames())
+                    ) // 2
+
+                    self._answer_pending = AnswerPendingRecord(
+                        packet.token, path_to_dest,
+                        expiring_time=self.env.now + estimated_rtt
+                    )
 
             if recv_ev.processed:
 
+                yield from self._send_ack(recv_ev.value)
                 self._handle_received(recv_ev.value)
 
     @singledispatchmethod
@@ -225,36 +258,42 @@ class MasterNode(ReThunderNode):
     @_handle_received.register(ResponsePacket)
     def _(self, packet):
 
+        answer_pending = self._answer_pending
+
+        if answer_pending.token != packet.token:
+            logger.warning('{} has received an answer with token {}. Current '
+                           'token is {}, ignoring'
+                           .format(self, packet.token, answer_pending.token))
+            return
+
         self._update_node_graph(packet)
         self._update_sptree()
         self._readdress_nodes()
 
         self.on_message_received(self, packet.payload, packet.payload_length)
+        self._answer_pending = None
 
-    def _make_request_packet(self, message, length, destination_addr):
+    def _make_request_packet(self, message, length, path_to_dest):
 
-        nodes = self._node_manager
         node_graph = self.node_graph
-
-        final_destination = nodes[destination_addr]
-        shortest_path = self._shortest_paths[final_destination]
 
         packet = RequestPacket()
         packet.payload = message
         packet.payload_length = length
 
-        destination_addr = final_destination.logic_address
+        destination_addr = path_to_dest[-1].logic_address
+
         address_stack = []           # type: List[int]
         tracer_stack = []            # type: List[Tracer]
 
         next_static_addressing_used = True
 
-        for next_node, node in iterblocks(reversed(shortest_path), 2, 1):
+        for next_node, node in iterblocks(reversed(path_to_dest), 2, 1):
 
             static_addressing_used = next_static_addressing_used
             next_static_addressing_used = False
 
-            if next_node.current_logic_address == -1:
+            if next_node.current_logic_address is None:
 
                 address_stack.append(next_node.static_address)
                 address_stack.append(next_node.logic_address)
@@ -297,11 +336,9 @@ class MasterNode(ReThunderNode):
                 destination_addr = next_node.current_logic_address
                 next_static_addressing_used = True
 
-            else:
-                if static_addressing_used:
-                    address_stack.append(next_node.current_logic_address)
-
-                    destination_addr = next_node.current_logic_address
+            elif static_addressing_used:
+                address_stack.append(next_node.current_logic_address)
+                destination_addr = next_node.current_logic_address
 
             if next_node.logic_address != next_node.current_logic_address:
                 tracer.new_address = True
@@ -313,6 +350,7 @@ class MasterNode(ReThunderNode):
                 tracer_stack[-1].offset += 1
 
         packet = RequestPacket()
+        packet.token = next(self._token_it)
 
         if tracer_stack[-1].offset == 0:
             tracer = tracer_stack.pop()
@@ -340,7 +378,7 @@ class MasterNode(ReThunderNode):
     def _update_node_graph(self, packet: ResponsePacket):
 
         node_graph = self.node_graph
-        message_path = self._current_message_path
+        message_path = self._answer_pending.path
 
         for node in message_path:
             node.current_logic_address = node.logic_address
