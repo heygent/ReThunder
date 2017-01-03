ed2ae0e5bb2f36a4a4c7084dd146fe0b17616721
Emanuele Gentiletti <emanuele.gentiletti@gmail.com>
2017-01-03 13:24:50 +0100

    Adds logger configuration to network

--- infrastructure/network.py
+++ infrastructure/network.py
@@ -1,8 +1,13 @@
+import logging
 import simpy
 import networkx as nx
 
 
 class Network:
+
+    _logging_formatter = logging.Formatter(
+        fmt="[{env.now:0>3}] {levelname} in {module}: {message}", style="{"
+    )
 
     def __init__(self, env: simpy.Environment=None, netgraph: nx.Graph=None,
                  transmission_speed: int=5):
@@ -10,3 +15,15 @@ class Network:
         self.env = env or simpy.Environment()
         self.netgraph = netgraph or nx.Graph()
         self.transmission_speed = transmission_speed
+
+    def configure_log_handler(self, handler):
+
+        handler.setFormatter(self._logging_formatter)
+
+        env = self.env
+
+        def env_filter(record):
+            record.env = env
+            return True
+
+        handler.addFilter(env_filter)
