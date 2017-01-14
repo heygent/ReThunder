97d9a73fa5af65f28355ed0c25b20aafc3fc75dc
Emanuele Gentiletti <emanuele.gentiletti@gmail.com>
2017-01-14 08:17:37 +0100

    Adds make_nodes

--- protocol/__init__.py
+++ protocol/__init__.py
@@ -1,2 +1,11 @@
 from .master_node import MasterNode
 from .slave_node import SlaveNode
+
+
+def make_nodes(network, n, master_on_received=None, slave_on_received=None):
+
+    nodes = [MasterNode(network, master_on_received)]
+    # noinspection PyTypeChecker
+    nodes.extend(SlaveNode(network, i, None, slave_on_received)
+                 for i in range(1, n))
+    return nodes
