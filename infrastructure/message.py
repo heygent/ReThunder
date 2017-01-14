6a835fc7da27cae7f5e16fc42d50624d775f493b
Emanuele Gentiletti <emanuele.gentiletti@gmail.com>
2017-01-14 10:58:33 +0100

    TransmittedMessage is now a namedtuple instead of inheriting from one

--- infrastructure/message.py
+++ infrastructure/message.py
@@ -10,16 +10,8 @@ CollisionSentinel = _CollisionSentinel()
 del _CollisionSentinel
 
 
-_TransmittedMessageBase = namedtuple('TransmittedMessage',
-                                     'value, transmission_delay, sender')
-
-
-class TransmittedMessage(_TransmittedMessageBase):
-
-    def __new__(cls, value, transmission_delay, sender=None):
-        return super(TransmittedMessage, cls).__new__(
-            cls, value, transmission_delay, sender
-        )
+TransmittedMessage = namedtuple('TransmittedMessage',
+                                'value, transmission_delay, sender')
 
 
 def make_transmission_delay(transmission_speed: float, msg_length: int) -> int:
