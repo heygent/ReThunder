f2ece311d9d80e2123f652920bf0cf53bbd055f5
Emanuele Gentiletti <emanuele.gentiletti@gmail.com>
2017-01-14 10:57:10 +0100

    Changes Node mechanism to use PreemptionFirstResource

--- infrastructure/node.py
+++ infrastructure/node.py
@@ -1,88 +1,17 @@
 import logging
 import weakref
-from typing import Any
+from typing import Any, Optional
 
 import simpy
 
-from utils.condition_var import ConditionVar, BroadcastConditionVar
+from utils.condition_var import BroadcastConditionVar
+from utils.preemption_first_resource import PreemptionFirstResource
 from utils.run_process_decorator import run_process
-from utils.updatable_process import UpdatableProcess
 from .message import (
     TransmittedMessage, CollisionSentinel, make_transmission_delay
 )
 
 logger = logging.getLogger(__name__)
-
-
-class NetworkState(UpdatableProcess):
-
-    occupy = UpdatableProcess.update
-
-    def __init__(self, env: simpy.Environment, owner=None):
-
-        super().__init__(env)
-
-        self.__network_is_free = ConditionVar(self.env)
-        self.__current_transmission_ended = BroadcastConditionVar(self.env)
-        self._current_message = None
-
-        if owner is None:
-            self._owner = lambda: None
-        else:
-            self._owner = weakref.ref(owner)
-
-    def wait_free_network_ev(self) -> simpy.Event:
-        return self.__network_is_free.wait()
-
-    def receive_current_transmission_ev(self) -> simpy.Event:
-        return self.__current_transmission_ended.wait()
-
-    @property
-    def network_is_busy(self):
-        return self._running
-
-    def _on_start(self, init_value):
-
-        self._current_message = init_value
-
-        self._stop_ev = self.env.timeout(
-            self._current_message.transmission_delay
-        )
-
-    def _on_update(self, update_value):
-
-        env = self.env
-
-        occupation_time_passed = (env.now - self._start_time)
-
-        occupation_time_left = (self._current_message.transmission_delay -
-                                occupation_time_passed)
-
-        logger.info(
-            f"{self._owner()!r}: A collision has happened between "
-            f"{self._current_message} and {update_value}"
-        )
-        assert occupation_time_left > 0
-
-        new_message = update_value  # type: TransmittedMessage
-
-        new_occupation_time_left = max(
-            new_message.transmission_delay, occupation_time_left
-        )
-
-        self._stop_ev = env.timeout(new_occupation_time_left)
-
-        self._current_message = TransmittedMessage(
-            CollisionSentinel,
-            occupation_time_passed + new_occupation_time_left,
-            None
-        )
-
-    def _on_stop(self, _):
-
-        message, self._current_message = self._current_message, None
-        self.__network_is_free.signal()
-        self.__current_transmission_ended.broadcast(message)
 
 
 class NetworkNode:
@@ -91,46 +20,75 @@ class NetworkNode:
 
     def __init__(self, network):
 
-        self.env = network.env  # type: simpy.Environment
+        self.env = env = network.env  # type: simpy.Environment
 
         self._netgraph = netgraph = weakref.proxy(network.netgraph)
-        self._network_state = NetworkState(self.env, self)
+        self._network_res = PreemptionFirstResource(env)
+        self._receive_current_transmission_cond = BroadcastConditionVar(env)
+        self._message_in_transmission: Optional[TransmittedMessage] = None
         self._transmission_speed = network.transmission_speed
 
         netgraph.add_node(self)
 
     @run_process
     def _send_to_network_proc(self, message_val: Any, message_len: int):
+        env = self.env
 
-        if self._network_state.network_is_busy:
-            yield self._network_state.wait_free_network_ev()
+        with self._network_res.request(preempt=False) as req:
+            yield req
+            # todo: Possibile punto di collisione
+            self._message_in_transmission = message_val
 
-        transmission_delay = make_transmission_delay(
-            self._transmission_speed, message_len
-        )
+            transmission_delay = make_transmission_delay(
+                self._transmission_speed, message_len
+            )
 
-        message = TransmittedMessage(message_val, transmission_delay, self)
+            message = TransmittedMessage(message_val, transmission_delay, self)
 
-        for bus in self._netgraph.neighbors(self):
-            bus.send_to_bus_proc(message)
+            for bus in self._netgraph.neighbors(self):
+                bus.send_to_bus_proc(message)
 
-        self._network_state.occupy(message)
+            try:
+                yield env.timeout(transmission_delay)
+            except simpy.Interrupt:
+                return
+
+            self._message_in_transmission = None
 
     @run_process
     def send_to_node_proc(self, message: TransmittedMessage):
-        # Make it a generator for eventual future changes that may require
-        # yielding events
-        if False:
-            yield
+        env = self.env
 
-        self._network_state.occupy(message)
+        if self._message_in_transmission is None:
+            self._message_in_transmission = message
+            transmission_delay = message.transmission_delay
+        else:
+            # todo correggi ritardo di trasmissione
+            transmission_delay = max(
+                message.transmission_delay,
+                self._message_in_transmission.transmission_delay
+            )
+            self._message_in_transmission = TransmittedMessage(
+                CollisionSentinel,
+                transmission_delay,
+                None
+            )
+        with self._network_res.request(preempt=True) as req:
+            try:
+                yield req
+                yield env.timeout(transmission_delay)
+            except simpy.Interrupt:
+                return
+
+        message = self._message_in_transmission
+        self._message_in_transmission = None
+
+        self._receive_current_transmission_cond.broadcast(message)
 
     @run_process
     def _receive_proc(self, timeout=None):
 
         env = self.env
-
-        received = self._network_state.receive_current_transmission_ev()
 
         if timeout is None:
             to = env.event()
@@ -143,19 +101,12 @@ class NetworkNode:
                 'timeout can be None, an integer or an event.'
             )
 
-        while True:
-            yield received or to
-            yield env.timeout(0)
+        received = self._receive_current_transmission_cond.wait()
 
-            assert any(e.processed for e in (received, to)), "Spurious wake"
+        yield received or to
 
-            if received.processed:
-                transmitted_msg = received.value
-                if transmitted_msg.sender is not self:
-                    return transmitted_msg.value
-                else:
-                    received = \
-                        self._network_state.receive_current_transmission_ev()
-            if to.processed:
-                return self.timeout_sentinel
-
+        if received.processed:
+            transmitted_msg = received.value
+            return transmitted_msg.value
+        if to.processed:
+            return self.timeout_sentinel
