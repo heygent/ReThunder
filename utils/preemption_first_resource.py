e2be6aa558911bcd5b62427aa5da61450a56a772
Emanuele Gentiletti <emanuele.gentiletti@gmail.com>
2017-01-14 16:14:32 +0100

    Improves readability of PreemptionFirstResource

--- utils/preemption_first_resource.py
+++ utils/preemption_first_resource.py
@@ -3,14 +3,20 @@ from simpy.resources.resource import Preempted
 
 
 class PreemptionFirstResource(PriorityResource):
+
     def _do_put(self, event):
+
         if len(self.users) >= self.capacity and event.preempt:
+
             # Check if we can preempt another process
-            preempt = sorted(self.users, key=lambda e: e.key)[-1]
-            if preempt.key >= event.key:
-                self.users.remove(preempt)
-                preempt.proc.interrupt(Preempted(by=event.proc,
-                                                 usage_since=preempt.time,
-                                                 resource=self))
+            preemptable = max(self.users, key=lambda e: e.key)
+
+            if preemptable.key >= event.key:
+
+                self.users.remove(preemptable)
+                preemptable.proc.interrupt(
+                    Preempted(by=event.proc, usage_since=preemptable.time,
+                              resource=self)
+                )
 
         return super(PreemptionFirstResource, self)._do_put(event)
