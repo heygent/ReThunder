39a73a5d5eaf7bc27d13ed88b1582443793c628f
Emanuele Gentiletti <emanuele.gentiletti@gmail.com>
2017-01-24 18:54:27 +0100

    Other minor stuff

--- protocol/__init__.py
+++ protocol/__init__.py
@@ -6,6 +6,6 @@ def make_nodes(network, n, master_on_received=None, slave_on_received=None):
 
     nodes = [MasterNode(network, master_on_received)]
     # noinspection PyTypeChecker
-    nodes.extend(SlaveNode(network, i, None, slave_on_received)
+    nodes.extend(SlaveNode(network, i, slave_on_received)
                  for i in range(1, n))
     return nodes
