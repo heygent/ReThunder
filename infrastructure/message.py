1d66e4507e42ea0ea7951fe963377fabfc9bd89a
Emanuele Gentiletti <emanuele.gentiletti@gmail.com>
2017-01-24 18:52:12 +0100

    Adds docs to infrastructure/message.py

--- infrastructure/message.py
+++ infrastructure/message.py
@@ -1,3 +1,14 @@
+"""
+Contiene strutture e funzioni riguardanti i messaggi trasmessi.
+
+La struttura che contiene le informazioni sui messaggi trasmessi è la
+namedtuple TransmittedMessage. Questa contiene il contenuto, il ritardo di
+trasmissione, e il nodo mittente del messaggio.
+
+CollisionSentinel è il valore a cui `TransmittedMessage.value` viene settato in
+caso di collisione del messaggio. I nodi possono testare se il messaggio
+ricevuto è stato soggetto di una collisione con l'operatore `is`.
+"""
 from collections import namedtuple
 
 
@@ -15,4 +26,11 @@ TransmittedMessage = namedtuple('TransmittedMessage',
 
 
 def make_transmission_delay(transmission_speed: float, msg_length: int) -> int:
+    """
+    Calcola il ritardo di trasmissione di un messaggio a partire dalla
+    velocità di trasmissione e dalla sua lunghezza.
+    :param transmission_speed: La velocità di trasmissione.
+    :param msg_length: La lunghezza del messaggio.
+    :return: Il ritardo di trasmissione.
+    """
     return max(int(msg_length / transmission_speed), 1)
