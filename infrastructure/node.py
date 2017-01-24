c49a0ca8c8c94f643fbd144d3fe61fff44139392
Emanuele Gentiletti <emanuele.gentiletti@gmail.com>
2017-01-24 18:53:04 +0100

    Changes to make PreemptionFirstResource no longer necessary

--- infrastructure/node.py
+++ infrastructure/node.py
@@ -5,7 +5,6 @@ from typing import Any, Optional
 import simpy
 
 from utils.condition_var import BroadcastConditionVar
-from utils.preemption_first_resource import PreemptionFirstResource
 from utils.simpy_process import simpy_process
 from .message import (
     TransmittedMessage, CollisionSentinel, make_transmission_delay
@@ -22,7 +21,7 @@ class NetworkNode:
 
         self._netgraph = netgraph = weakref.proxy(network.netgraph)
         self._transmission_speed = network.transmission_speed
-        self._network_res = PreemptionFirstResource(env)
+        self._current_occupy_proc = None
         self._receive_current_transmission_cond = BroadcastConditionVar(env)
         self._message_in_transmission: Optional[TransmittedMessage] = None
         self._last_transmission_start = None
@@ -50,53 +49,64 @@ class NetworkNode:
 
     def _occupy(self, message: TransmittedMessage, in_transmission):
         env = self.env
+        this_proc = env.active_process
+
+        if in_transmission:
+            while True:
+                if self._current_occupy_proc is None:
+                    break
+                yield self._current_occupy_proc
+        else:
+            if self._current_occupy_proc is not None:
+                self._current_occupy_proc.interrupt()
+
+        self._current_occupy_proc = this_proc
 
         last_transmission_start = self._last_transmission_start
         self._last_transmission_start = env.now
 
-        with self._network_res.request(preempt=not in_transmission) as req:
-            yield req
-
-            if self._message_in_transmission is None:
-                self._message_in_transmission = message
-                to_wait = message.transmission_delay
-            else:
-                logger.warning(f"{self}: A collision has happened between "
-                               f"{message} and {self._message_in_transmission}")
-
-                if in_transmission:
-                    logger.error(
-                        f"{self}: Collision has happened as a consequence of a "
-                        f"transmission by the node. This should not happen."
-                    )
-
-                last_occupation_time = env.now - last_transmission_start
-
-                remaining_occupation_time = (
-                    self._message_in_transmission.transmission_delay -
-                    last_occupation_time
-                )
-
-                to_wait = max(message.transmission_delay,
-                              remaining_occupation_time)
-
-                self._message_in_transmission = TransmittedMessage(
-                    CollisionSentinel,
-                    last_occupation_time + to_wait,
-                    None
-                )
+        if self._message_in_transmission is None:
+            self._message_in_transmission = message
+            to_wait = message.transmission_delay
+        else:
+            logger.warning(f"{self}: A collision has happened between "
+                           f"{message} and {self._message_in_transmission}")
 
             if in_transmission:
-                for bus in self._netgraph.neighbors(self):
-                    bus.send_process(message)
+                logger.error(
+                    f"{self}: Collision has happened as a consequence of a "
+                    f"transmission by the node. This should not happen."
+                )
 
-            try:
-                yield env.timeout(to_wait)
-            except simpy.Interrupt:
-                return
+            last_occupation_time = env.now - last_transmission_start
 
-            message = self._message_in_transmission
-            self._message_in_transmission = None
+            remaining_occupation_time = (
+                self._message_in_transmission.transmission_delay -
+                last_occupation_time
+            )
 
-            if not in_transmission:
-                self._receive_current_transmission_cond.broadcast(message.value)
+            to_wait = max(message.transmission_delay,
+                          remaining_occupation_time)
+
+            self._message_in_transmission = TransmittedMessage(
+                CollisionSentinel,
+                last_occupation_time + to_wait,
+                None
+            )
+
+        if in_transmission:
+            for bus in self._netgraph.neighbors(self):
+                bus.send_process(message)
+
+        try:
+            yield env.timeout(to_wait)
+        except simpy.Interrupt:
+            return
+
+        message = self._message_in_transmission
+        self._message_in_transmission = None
+
+        self._current_occupy_proc = None
+
+        if not in_transmission:
+            self._receive_current_transmission_cond.broadcast(message.value)
